069a6ab1a922bab64c9eb390acd113da282fab3b
HYP3R_X <146062554+adarsh40parihar@users.noreply.github.com>
2023-11-29 19:19:00 +0530

    Create code.cpp

---
+++ SPF and Sieve Of Eratosthenes/code.cpp
@@ -0,0 +1,21 @@
+    int n = 1e6;
+    vi spf(n);
+    f(i,0,n){
+        spf[i]=i;
+    }
+    for(int i = 2;i*i<=n;i++){
+        if(spf[i]==i){
+            for(int j=i*i;j<=n;j+=i){
+                if(spf[j]==j)
+                 spf[j]=i;
+            }
+        }
+    }
+    int t;
+    cin>>t;
+    while(t--){
+        int k;
+        cin>>k;
+        cout<<spf[k]<<endl;
+    }
+}
