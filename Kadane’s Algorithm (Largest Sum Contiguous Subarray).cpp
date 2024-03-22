4e07806a3b857fec759c6e6d97d9a656fcb589b4
HYP3R_X <146062554+adarsh40parihar@users.noreply.github.com>
2024-03-23 00:54:01 +0530

    Kadane’s Algorithm (Largest Sum Contiguous Subarray).cpp

--- Kadane’s Algorithm (Largest Sum Contiguous Subarray).cpp
+++ Kadane’s Algorithm (Largest Sum Contiguous Subarray).cpp
@@ -1,6 +1,6 @@
 int maxSubArray(vector<int>& nums) {
-    int maxsum=nums[0], sum=0;                                //If we want atleast one element as a answer from the all negative array(in sort return minimum
-    //for minimum sub array sum is zero use maxsum=0;         // negative element)
+    int maxsum=nums[0], sum=0;                                //If we want atleast one element as a answer from the all negative array 
+    //for minimum sub array sum is zero use maxsum=0;         // (in sort return minimum negative element of whole array)
     for(int i=0;i<nums.size();i++){                
         sum+=nums[i];
         maxsum=max(maxsum,sum);
